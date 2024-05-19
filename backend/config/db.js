af65ffc1926b0e1a2d62dae9eab8355f1c01ae61
Dhere02 <148745942+Dhere02@users.noreply.github.com>
2024-05-19 17:53:48 +0000

    running

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 import mongose from 'mongoose'
 import colors from 'colors'
 
+const MONGO_URI="mongodb+srv://divya:1234@cluster0.f6wltxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // a mongoose stuf (mongoose.connect ....) return always a promise
 const connectDB = async () => {
     try{
-        const conn = await mongose.connect(process.env.MONGO_URI,{
+        const conn = await mongose.connect(MONGO_URI,{
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true
